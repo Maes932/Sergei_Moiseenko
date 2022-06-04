5baef88b33961ad69744ff2dc6971f38f6f45e32
Siarhei_Moiseenka <347_84_97@mail.ru>
2022-06-04 22:26:56 +0300

    commit

---
+++ Python/lesson3/task2.py
@@ -0,0 +1,23 @@
+my_str = 'y=34+9'
+my_str = my_str.replace('=', ' ')
+my_str = my_str.replace('+', ' ')
+new_str = my_str.split(' ')
+k = new_str[1]
+b = new_str[2]
+c = k.replace('x', '')
+if  (c,b):
+    0 >= 99
+    print('k=', c, 'b=', b)
+
+  
+         
+
+
+    
+
+
+
+
+ 
+
+
