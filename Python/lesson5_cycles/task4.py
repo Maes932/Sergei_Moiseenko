ce48dbe9657663588561443a59af7fe63a92fd42
Siarhei_Moiseenka <347_84_97@mail.ru>
2022-06-20 00:33:40 +0300

    commit

--- Python/lesson5_cycles/task4.py
+++
@@ -1,8 +0,0 @@
-a = {}
-for _ in range(int(input())):
-    s = input().split()
-    country, cityes = s[0], s[1:]
-    for city in cityes:                           #Города
-        a[city] = country
-for i in range(int(input())):
-    print(a[input()])
