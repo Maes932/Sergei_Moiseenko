6386ae98bc57dede9d99c97c4c5ebf9fb2ca0944
Siarhei_Moiseenka <347_84_97@mail.ru>
2022-06-20 00:29:16 +0300

    commit

--- Python/lesson5_Sets_Gen/task3.py
+++ Python/lesson5_Sets_Gen/task3.py
@@ -1,9 +1,32 @@
 set_ten = {'Evans', 'Stone', 'Roberts', 'Mills', 'Lewis', 'Morgan'}
 set_dota = {'Grant', 'Morgan', 'Evans', 'Mills', 'Jackson', 'Bradley'}
 set_litr = {'Grant', 'Morgan', 'Evans', 'Gibson', 'Jackson', 'Barlow', 'Adams'}
+print()
 print('Увлекаются всем:', set_ten & set_dota & set_litr)
-print('Увлекаются только дотой и литрболом:', )
+print()
+except_ten = set_dota & set_litr - set_ten
+print('Увлекаются только дотой и литрболом:', except_ten)
+print()
+except_dota = set_litr & set_ten - set_dota
+print('Увлекаются только тенисом и литрболом:', except_dota)
+print()
+except_litr = set_ten & set_dota - set_litr
+print('Увлекаются только тенисом и дотой:', except_litr)
+print()
+only_dota = set_dota - set_litr - set_ten
+print('Увлекаются только дотой:', only_dota)
+print()
+only_litr = set_litr - set_dota - set_ten
+print('Увлекаются только литрболом:', only_litr)
+print()
+only_ten = set_ten - set_dota - set_litr
+print('Увлекаются только тенисом:', only_ten)
+print()
 
-# В работе!
+
+
+
+
+
 
 
--- Python/lesson5_cycles/task1.py
+++ Python/lesson5_cycles/task1.py
@@ -1,6 +1,6 @@
 while True:
-    n = int(input(' :'))
+    num = int(input(' :'))
     fib = [0, 1]
-    while len(fib) <= n:                #Фибоначчи 
+    while len(fib) <= num:                #Фибоначчи 
         fib.append(fib[-2]+fib[-1])
     print(fib[-1])
